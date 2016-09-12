e9de45a8e24cb46b0b2331e521d3f664591122ce
Ben Arunski <ben.arunski@capstonec.com>
2016-09-12 14:29:23 -0500

    compare classifications REFACTORED

--- 20160912_Capstone_compareClassifications/CaseManagementService.java
+++ 20160912_Capstone_compareClassifications/CaseManagementService.java
@@ -1,358 +1,47 @@
 
-public boolean isNewClassificationHigher(Integer currentClassificationCd,
-		Integer newClassificationCd) {
+private static final Map<Integer, Integer> classificationHeight = new HashMap<>();
 
-	boolean isHigher = false;
-	if (currentClassificationCd != null && newClassificationCd != null) {
-		switch (currentClassificationCd) {
-		case NpacsConstants.CLASSIFICATION_CBI:
-		case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-		case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-		case NpacsConstants.CLASSIFICATION_SSAS:
-		case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-		case NpacsConstants.CLASSIFICATION_WEC:
-		case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			isHigher = false;
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-				isHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_LOW:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-				isHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_LOW:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_LOW:
-				isHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_ADMIN_OVERRIDE_VL:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_LOW:
-			case NpacsConstants.CLASSIFICATION_CBR_LOW:
-				isHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_JCBI:
-		case NpacsConstants.CLASSIFICATION_JSH:
-		case NpacsConstants.CLASSIFICATION_3_B:
-		case NpacsConstants.CLASSIFICATION_PSC_JUVENILE:
-			isHigher = false;
-			break;
-		case NpacsConstants.CLASSIFICATION_JCBR_LOW_MODERATE:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_JCBI:
-			case NpacsConstants.CLASSIFICATION_JSH:
-			case NpacsConstants.CLASSIFICATION_3_B:
-			case NpacsConstants.CLASSIFICATION_PSC_JUVENILE:
-				isHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_JCBR_LOW:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_JCBI:
-			case NpacsConstants.CLASSIFICATION_JSH:
-			case NpacsConstants.CLASSIFICATION_3_B:
-			case NpacsConstants.CLASSIFICATION_PSC_JUVENILE:
-			case NpacsConstants.CLASSIFICATION_JCBR_LOW_MODERATE:
-				isHigher = true;
-				break;
-			}
-			break;
-		}
-	}
+static {
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_CBI, 50);
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_SEX_OFFENDER, 50);
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE, 50);
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_SSAS, 50);
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_PSC_ADULT, 50);
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_WEC, 50);
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_CBI_DUI, 50);
 
-	return isHigher;
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH, 40);
+
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_CBR_MEDIUM_LOW, 30);
+
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_CBR_LOW, 20);
+
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_CBR_ADMIN_OVERRIDE_VL, 10);
+
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_JCBI, 30);
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_JSH, 30);
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_3_B, 30);
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_PSC_JUVENILE, 30);
+
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_JCBR_LOW_MODERATE, 20);
+
+	classificationHeight.put(NpacsConstants.CLASSIFICATION_JCBR_LOW, 10);
 }
 
-public boolean isNewClassificationEqualOrLower(
-		Integer currentClassificationCd, Integer newClassificationCd) {
-	boolean isEqualOrLower = false;
-	if (currentClassificationCd != null && newClassificationCd != null) {
-		switch (currentClassificationCd) {
-		case NpacsConstants.CLASSIFICATION_CBI:
-		case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-		case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-		case NpacsConstants.CLASSIFICATION_SSAS:
-		case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-		case NpacsConstants.CLASSIFICATION_WEC:
-		case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			isEqualOrLower = true;
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-				isEqualOrLower = false;
-				break;
-			default:
-				isEqualOrLower = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_LOW:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-				isEqualOrLower = false;
-				break;
-			default:
-				isEqualOrLower = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_LOW:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_LOW:
-				isEqualOrLower = false;
-				break;
-			default:
-				isEqualOrLower = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_ADMIN_OVERRIDE_VL:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_LOW:
-			case NpacsConstants.CLASSIFICATION_CBR_LOW:
-				isEqualOrLower = false;
-				break;
-			default:
-				isEqualOrLower = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_JCBI:
-		case NpacsConstants.CLASSIFICATION_JSH:
-		case NpacsConstants.CLASSIFICATION_3_B:
-		case NpacsConstants.CLASSIFICATION_PSC_JUVENILE:
-			isEqualOrLower = true;
-			break;
-		case NpacsConstants.CLASSIFICATION_JCBR_LOW_MODERATE:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_JCBI:
-			case NpacsConstants.CLASSIFICATION_JSH:
-			case NpacsConstants.CLASSIFICATION_3_B:
-			case NpacsConstants.CLASSIFICATION_PSC_JUVENILE:
-				isEqualOrLower = false;
-				break;
-			default:
-				isEqualOrLower = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_JCBR_LOW:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_JCBI:
-			case NpacsConstants.CLASSIFICATION_JSH:
-			case NpacsConstants.CLASSIFICATION_3_B:
-			case NpacsConstants.CLASSIFICATION_PSC_JUVENILE:
-			case NpacsConstants.CLASSIFICATION_JCBR_LOW_MODERATE:
-				isEqualOrLower = false;
-				break;
-			default:
-				isEqualOrLower = true;
-				break;
-			}
-			break;
-		}
-	}
-
-	return isEqualOrLower;
+public boolean isNewClassificationHigher(Integer currentClassificationCd, Integer newClassificationCd) {
+	return currentClassificationCd != null
+		   && newClassificationCd != null
+		   && classificationHeight.get(newClassificationCd) > classificationHeight.get(currentClassificationCd);
 }
 
-public boolean isNewClassificationEqualOrHigher(
-		Integer currentClassificationCd, Integer newClassificationCd) {
+public boolean isNewClassificationEqualOrLower(Integer currentClassificationCd, Integer newClassificationCd) {
+	return currentClassificationCd != null
+		   && newClassificationCd != null
+		   && classificationHeight.get(newClassificationCd) <= classificationHeight.get(currentClassificationCd);
+}
 
-	boolean isEqualOrHigher = false;
-	if (currentClassificationCd != null && newClassificationCd != null) {
-		switch (currentClassificationCd) {
-		case NpacsConstants.CLASSIFICATION_CBI:
-		case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-		case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-		case NpacsConstants.CLASSIFICATION_SSAS:
-		case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-		case NpacsConstants.CLASSIFICATION_WEC:
-		case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-				isEqualOrHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-				isEqualOrHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_LOW:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_LOW:
-				isEqualOrHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_LOW:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_LOW:
-			case NpacsConstants.CLASSIFICATION_CBR_LOW:
-				isEqualOrHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_CBR_ADMIN_OVERRIDE_VL:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_CBI:
-			case NpacsConstants.CLASSIFICATION_SEX_OFFENDER:
-			case NpacsConstants.CLASSIFICATION_DOMESTIC_VIOLENCE:
-			case NpacsConstants.CLASSIFICATION_SSAS:
-			case NpacsConstants.CLASSIFICATION_PSC_ADULT:
-			case NpacsConstants.CLASSIFICATION_WEC:
-			case NpacsConstants.CLASSIFICATION_CBI_DUI:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_HIGH:
-			case NpacsConstants.CLASSIFICATION_CBR_MEDIUM_LOW:
-			case NpacsConstants.CLASSIFICATION_CBR_LOW:
-			case NpacsConstants.CLASSIFICATION_CBR_ADMIN_OVERRIDE_VL:
-				isEqualOrHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_JCBI:
-		case NpacsConstants.CLASSIFICATION_JSH:
-		case NpacsConstants.CLASSIFICATION_3_B:
-		case NpacsConstants.CLASSIFICATION_PSC_JUVENILE:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_JCBI:
-			case NpacsConstants.CLASSIFICATION_JSH:
-			case NpacsConstants.CLASSIFICATION_3_B:
-			case NpacsConstants.CLASSIFICATION_PSC_JUVENILE:
-				isEqualOrHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_JCBR_LOW_MODERATE:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_JCBI:
-			case NpacsConstants.CLASSIFICATION_JSH:
-			case NpacsConstants.CLASSIFICATION_3_B:
-			case NpacsConstants.CLASSIFICATION_PSC_JUVENILE:
-			case NpacsConstants.CLASSIFICATION_JCBR_LOW_MODERATE:
-				isEqualOrHigher = true;
-				break;
-			}
-			break;
-		case NpacsConstants.CLASSIFICATION_JCBR_LOW:
-			switch (newClassificationCd) {
-			case NpacsConstants.CLASSIFICATION_JCBI:
-			case NpacsConstants.CLASSIFICATION_JSH:
-			case NpacsConstants.CLASSIFICATION_3_B:
-			case NpacsConstants.CLASSIFICATION_PSC_JUVENILE:
-			case NpacsConstants.CLASSIFICATION_JCBR_LOW_MODERATE:
-			case NpacsConstants.CLASSIFICATION_JCBR_LOW:
-				isEqualOrHigher = true;
-				break;
-			}
-			break;
-		}
-	}
-
-	return isEqualOrHigher;
+public boolean isNewClassificationEqualOrHigher(Integer currentClassificationCd, Integer newClassificationCd) {
+	return currentClassificationCd != null
+		   && newClassificationCd != null
+		   && classificationHeight.get(newClassificationCd) >= classificationHeight.get(currentClassificationCd);
 }
